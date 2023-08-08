7983fa7d1092d067ca1c7eae32ec9f4afc9883bf
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-08-08 13:44:18 +0900

    Update Team.js

--- commands/Gernal/Team.js
+++ commands/Gernal/Team.js
@@ -1,60 +1,46 @@
+const {
+  SlashCommandBuilder,
+  ChannelType,
+  PermissionFlagsBits,
+} = require("discord.js");
+const joinLog_Schema = require("../../models/joinLog");
 
-const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
-    data: new SlashCommandBuilder()
-      .setName("팀짜기")
-      .setDescription("팀을 나눠줍니다.")
-      .addIntegerOption((teamf) =>
-      teamf.setName("숫자")
-          .setDescription("팀짜기 숫자를 적어주세요!")
-          .setMinValue(1)
-          .setRequired(true)
-      ),
-      async execute(interaction){
-          await interaction.deferReply();
-      /**
-     * @param {import("discord.js").Client} client
-     */{
-      var arg = interaction.options.getInteger("숫자");
-      var membersArray = [];
-      channel = interaction.channel.id
-      let voiceChannel = interaction.member.voice.channel;
-      if (!voiceChannel) {return interaction.editReply('음성 채널에 접속해주세요')}
-      for (let member of voiceChannel.members) {
-        membersArray.push('<@' + member[1].user.id + '>')
-      }
-      var teamArray = makeTeam(membersArray, Math.floor(arg));
-  
-      const result = teamArray.map((team, idx) => `${idx + 1}팀: ${team.join(', ')}`);
-    
-      await interaction.editReply(result.join('\n'));
-    }
-  
-    function makeTeam(members, number) {
-  
-      const generateRandomKey = () => Math.floor(Math.random() * 1000);
-    
-      const membersWithKey = members.map(member => ({
-        name: member,
-        key: generateRandomKey(),
-      }));
-    
-      const sortResult = membersWithKey.sort((a, b) => {
-        return a.key >= b.key ? 1 : -1;
-      });
-    
-      const membersWithoutKey = sortResult.map(res => res.name);
-    
-      const teamResult = [];
-    
-      for (let i = 0; i < members.length; i++) {
-        const piece = [...membersWithoutKey].slice(i, i + number);
-        teamResult.push(piece);
-    
-        i += number - 1;
-      }
-    
-      return teamResult;
-    }
-  }
-}
+  data: new SlashCommandBuilder()
+    .setName("입장로그")
+    .setDescription("서버 입장로그를 설정합니다")
+    .addChannelOption((f) =>
+      f
+        .setName("채널")
+        .setDescription("입장로그가 전송될 채널을 선택해 주세요")
+        .setRequired(true)
+        .addChannelTypes(ChannelType.GuildText)
+    )
+    .addStringOption((f) =>
+      f
+        .setName("환영메시지")
+        .setDescription("환영 메시지를 입력해 주세요")
+        .setRequired(true)
+        .setMinLength(10)
+        .setMaxLength(4000)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  /**
+   *
+   * @param {import("discord.js").ChatInputCommandInteraction} interaction
+   */
+  async execute(interaction) {
+    const option_channel = interaction.options.getChannel("채널");
+    const option_welcomeText = interaction.options.getString("환영메시지");
+
+    await joinLog_Schema.updateOne(
+      { guildId: interaction.guild.id },
+      { channelId: option_channel.id, welcomeText: option_welcomeText },
+      { upsert: true }
+    );
+
+    interaction.reply({
+      content: `**입장로그가 ${option_channel}에 표시됩니다**`,
+    });
+  },
+};
