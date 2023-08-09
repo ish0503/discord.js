802500558cf6a5c0d621cbb74e16c73d12350a5b
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-08-09 10:22:03 +0900

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "messageCreate",
   once: false,
   execute(message) {
-    if (message.content == "채팅봇") {
+    if (message.content == "채팅봇" || message.content == "새늅봇") {
       message.reply({ content: `**접니다!**` });
     }
   },
