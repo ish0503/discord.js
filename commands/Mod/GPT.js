60541c2fb6c114f8af5898a5ec15984ff7451f63
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-08-11 18:07:48 +0900

    Update GPT.js

--- commands/Mod/GPT.js
+++ commands/Mod/GPT.js
@@ -5,36 +5,7 @@ const ket222 = "BlbkFJlbUrFogBcHyz8BarKwew"
 const configuration = new Configuration
   ({ apiKey: ket111 + ket222 });
 
-//var cooldown = false
-
 openai = new OpenAIApi(configuration)
-
-// function chapGPT(prompt, prompt2) {
-//    const response = await openai.createChatCompletion({
-//     model: "gpt-3.5-turbo",
-//     messages: [{ role: "user", content: prompt2 + " (Reply in 2000 words or less)" }],
-//     });
-//     //if (response["data"]["choices"][0]["message"]["content"] != "Promise { <pending> }")  {
-//     console.log(response["data"]["choices"][0]["message"]["content"])
-//     prompt.channel.send(prompt2 + "(이)라는 질문에 답변: " + "**" + response["data"]["choices"][0]["message"]["content"] + "**")
-//     cooldown = false
-// }
-
-const chapGPT = async (prompt, prompt2) => {
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt2 + " (Reply in 1000 words or less)" }],
-    });
-    //if (response["data"]["choices"][0]["message"]["content"] != "Promise { <pending> }")  {
-    console.log(response["data"]["choices"][0]["message"]["content"])
-    prompt.channel.send(prompt2 + "(이)라는 질문에 답변: " + "**" + response["data"]["choices"][0]["message"]["content"] + "**")
-    //cooldown = false
-  //prompt.reply(response["data"]["choices"][0]["message"]["content"]);
-  //prompt.channel.stopTyping();
-  // return response["data"]["choices"][0]["message"]["content"];
-  // };
-  //return response["data"]["choices"][0]["message"]["content"];
-};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -51,8 +22,7 @@ module.exports = {
 
   // @param {import("discord.js").ChatInputCommandInteraction} interaction
   async execute(interaction) {
-    await interaction.deferReply( {ephemeral: true} );
-    //if (cooldown) return
+    await interaction.deferReply(); //{ephemeral: true}
     const reason_option = interaction.options.getString("메시지");
 
     try {
@@ -69,21 +39,10 @@ module.exports = {
       .setColor("Blue")
       .setDescription(res["data"]["choices"][0]["text"])
 
-      //await interaction.reply({ embeds: [embed] });
-      //await interaction.channel.send({ embeds: [embed] });
       await interaction.editReply({ embeds: [embed] });
       
-     // cooldown = true
-      // interaction.reply("잠시만 기다려주세요..")
-      //interaction.channel.startTyping();
-      // console.log(reason_option)
-      // chapGPT(interaction, reason_option);
     } catch (error) {
       return await interaction.editReply({content: `오류 발생 **${error.response.status}**`, ephemeral: true})
-      // cooldown = false
-      // return interaction.reply({
-      //   content: `**메시지전송에 실패했습니다**`,
-      // });
     }
   },
 };
