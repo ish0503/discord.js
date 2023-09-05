3aba3fda2915a3fc75ce00df976e80205f511aed
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-05 22:04:56 +0900

    Delete events/messageCreate.js

--- events/messageCreate.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "messageCreate",
-  once: false,
-  execute(message) {
-    if (message.content == "채팅봇" || message.content == "새늅봇") {
-      message.reply({ content: `**접니다!**` });
-    }
-  },
-};
