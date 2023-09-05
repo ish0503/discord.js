15f153bc3ae540c468c20c17993da8f36a670283
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-05 19:43:52 +0900

    Update GPTa.js

--- commands/Mod/GPTa.js
+++ commands/Mod/GPTa.js
@@ -1,13 +1,12 @@
 const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
-//const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 
-//const configuration = new Configuration
-  //({ apiKey: process.env.CHATGPTKEY });
-import { BingChat } from 'bing-chat'
+const configuration = new Configuration
+  ({ apiKey: process.env.CHATGPTKEY });
 
-//openai = new OpenAIApi(configuration)
+openai = new OpenAIApi(configuration)
 
-var history1 = []
+var history = []
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -28,32 +27,37 @@ module.exports = {
     const reason_option = interaction.options.getString("메시지");
 
     try {
-        history1.push({"role": "user", "content": reason_option})
-            //const response = await openai.createChatCompletion({
-              //  model: "gpt-3.5-turbo",
-              //  messages: history1,
-            //});
-      
-            const api = new BingChat({
-                cookie: process.env.BING_COOKIE
-            })
-         
-            const res = await api.sendMessage(reason_option)
-            console.log(res.text)
-      
-            output = res.text
+      if (reason_option == "exitMessage" || history.length > 50){
+            history = [];
+            const embed = new EmbedBuilder()
+            .addFields(
+                    { name: "Lenin", value: `**대화가 길어지거나 다른 요인때문에 기억이 삭제되었습니다. 다시 질문해주세요.**`, inline: true },
+            )
+            .setTitle(reason_option) 
+            .setColor("Blue")
         
-            history1.push({"role": "assistant", "content": output})
+            await interaction.editReply({ embeds: [embed] })
+      } else {
+        history.push({"role": "user", "content": reason_option})
+            const response = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: history,
+            });
+      
+            output = response["data"]["choices"][0]["message"]["content"]
+        
+            history.push({"role": "assistant", "content": output})
             console.log(output)
       
             const embed = new EmbedBuilder()
             .addFields(
-                    { name: "Ai", value: `**${res.text}**`, inline: true },
+                    { name: "Ai", value: `**${response["data"]["choices"][0]["message"]["content"]}**`, inline: true },
             )
             .setTitle(reason_option) 
             .setColor("Blue")
       
             await interaction.editReply({ embeds: [embed] });
+      }
     } catch (error) {
       console.log(error.response)
       return await interaction.editReply({content: `오류 발생 **${error.response.status}**, **${error.response.statusText} 이같은 오류가 계속 발생한다면, 문의 넣어주세요.**`, ephemeral: true})
