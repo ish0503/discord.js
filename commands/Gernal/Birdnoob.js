bcd71e993ab381a4aae00e53f6062f0354f8fe42
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-05 21:43:11 +0900

    Create Birdnoob.js

---
+++ commands/Gernal/Birdnoob.js
@@ -0,0 +1,15 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("새늅교")
+    .setDescription("새늅교에 대한 정보를 보여줍니다")
+    async execute(interaction) {
+      
+            interaction.reply("새늅교는 9월 5일 창설되었다.")
+  interaction.channel.send("사이트는 https://birdnoob.netlify.app/ 이며, 앞으로 더 내용이 추기될것이다.")
+        }).catch(e => {
+            return interaction.channel.send(`오류가 발생했습니다 ${e}`)
+        })
+    }
+              }
