f0b73018617ff4869c111e2e0c6ed23631c9a38c
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-06 19:07:21 +0900

    Update hasegi.js

--- commands/Gernal/hasegi.js
+++ commands/Gernal/hasegi.js
@@ -1,15 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js')
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("site")
-        .setDescription("사이트의 정보입니다.") 
-    .addStringOption(options => options
-        .setName("aa")
-        .setDescription("aa")
-    )
-    async execute(interaction) {
-        await interaction.deferReply();
-        interaction.channel.send("**https://birdmeow.netlify.app/**")
-    },
+  data: new SlashCommandBuilder()
+    .setName("site")
+    .setDescription("사이트의 정보입니다")
+  async execute(interaction) {
+    try {
+      interaction.reply({ content: "https://birdmeow.netlify.app/" });
+    } catch (error) {
+      interaction.reply({ content: "전송하지 못했습니다" });
+    }
+  },
 };
