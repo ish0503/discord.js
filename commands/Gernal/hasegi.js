9659c19aeca139a5a5e3ae32f8bac26a7743b1cd
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-06 19:14:40 +0900

    Update hasegi.js

--- commands/Gernal/hasegi.js
+++ commands/Gernal/hasegi.js
@@ -5,11 +5,6 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName("site")
     .setDescription("사이트의 정보입니다")
-    .addStringOption(options => options
-        .setName("검색어")
-         .setDescription("검색어를 입력해주세요.")
-         .setRequired(true)
-    ),
     async execute(interaction) {
         await interaction.deferReply();
 
