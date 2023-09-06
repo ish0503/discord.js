cc0ce1567b416a05ef90edff4f0c691f61eef130
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-06 19:19:55 +0900

    Create gpt.js

---
+++ commands/Gernal/gpt.js
@@ -0,0 +1,40 @@
+const { Configuration, OpenAIApi } = require('openai');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+
+const configuration = new Configuration({
+  apiKey: process.env.CHATGPTKEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("gpt")
+    .setDescription("ai와 대화하세요")
+    .addStringOption(options => options
+        .setName("검색어")
+        .setDescription("검색어를 입력해주세요.")
+        .setRequired(true)
+    ),
+    async execute(interaction) {
+      try {
+      const args = interaction.options.getString("검색어")
+
+        await interaction.deferReply();
+
+        const result = await openai
+        .createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: conversationLog,
+        // max_tokens: 256, // limit token usage
+      })
+      .catch((error) => {
+        console.log(`OPENAI ERR: ${error}`);
+      });
+      interaction.reply(result.data.choices[0].message);
+      }  catch (error) {
+      console.log(`ERR: ${error}`);
+         interaction.reply(`ERR: ${error}`);
+      }
+    }
+}
