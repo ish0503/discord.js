af9bdac983d6366dff8894b2a38aa03ea3bd8c68
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-07 20:08:38 +0900

    Update gpt.js

--- commands/Gernal/gpt.js
+++ commands/Gernal/gpt.js
@@ -38,20 +38,20 @@ module.exports = {
         // max_tokens: 256, // limit token usage
       })
 	console.log(result.data.usage.total_tokens + "토큰 사용")
-	translate(result.data.choices[0].message.content, {from:'en', to:'ko'}).then(res => {
+	//translate(result.data.choices[0].message.content, {from:'en', to:'ko'}).then(res => {
     const embed = new EmbedBuilder()
     .setTitle(`${args}에 대한 답변`)
     .setDescription(`**${result.data.choices[0].message.content}**`)
     .setFooter({ text: `유저 이름 : ${interaction.user.username}(${interaction.user.globalName}), ID: ${interaction.user.id}` })
     .setColor(0xFFFF00)
     .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
-    .setImage('https://cdn.discordapp.com/avatars/989433815539777546/5da15b220fdc3e27072e4c4a9ffa4987.webp?size=512');
+    .setImage('https://cdn.discordapp.com/avatars/989433815539777546/5da15b220fdc3e27072e4c4a9ffa4987.webp?size=256');
 
   interaction.editReply({ embeds: [embed] });
-  }).catch(err => {
-    console.log(`ERR: ${err}`);
-    interaction.editReply(`ERR: ${err}`);
-  })
+  // }).catch(err => {
+  //   console.log(`ERR: ${err}`);
+  //   interaction.editReply(`ERR: ${err}`);
+  // })
       //interaction.channel.send(args+"에 대한 답변: "+result.data.choices[0].message.content);
       }  catch (error) {
       console.log(`ERR: ${error}`);
