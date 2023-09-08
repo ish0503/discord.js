ee266679112705785caa3ce6a0f684613e70833d
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-09 07:02:41 +0900

    Update makeitem.js

--- commands/Gambling/makeitem.js
+++ commands/Gambling/makeitem.js
@@ -37,12 +37,12 @@ module.exports = {
 
         await gambling_Schema.updateOne(
             {userid: interaction.user.id},
-            {
+            {$push: {
                 hashtags : [
                     { "name": args, "value": 1 },
                ],
-            //},
-             cooltime: Date.now()},
+            }},
+             {cooltime: Date.now()},
             {upsert:true}
         );
 
