3b1c2d103f0e9da5afbe6fda0960bfd7cbb9d563
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-09 00:22:02 +0900

    Update upgrade.js

--- models/upgrade.js
+++ models/upgrade.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const SchemaF = new Schema({
   userid: String,
-  hashtags : { type : Array , "default" : [] },
+  hashtags : { type : Array , "default" : [{ "name": String, "value": Number }] },
   cooltime: String,
 })
 
