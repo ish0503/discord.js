11dcf9afd7777446b43fc043cdfece2ca43746b5
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-08 16:14:05 +0900

    Add files via upload

---
+++ commands/Gernal/ping.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+
+    .setName("핑")
+    .setDescription("봇과 자신의 핑 확인"),
+    /**
+     * 
+     * @param {import(*discord.js*).ChatInputCommandInteraction} interaction
+     */
+    async execute(interaction){
+        await interaction.deferReply()
+
+        const msg = await interaction.fetchReply()
+        
+        const embed = new EmbedBuilder()
+        .setTitle(`🏓퐁! 응답이 왔어요!`)
+        .setDescription(`🏓 커멘드 핑: ${msg.createdTimestamp - interaction.createdTimestamp}ms\n🏓 봇 핑 : ${interaction.client.ws.ping}ms`)
+        .setColor(0xFFFF00)
+
+        interaction.editReply({ embeds: [embed] })
+    }
+}
