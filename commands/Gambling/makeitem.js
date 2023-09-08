2ff89f92c54fccfe0ecd29029ef947cfb7a59f22
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-09 00:30:52 +0900

    Update makeitem.js

--- commands/Gambling/makeitem.js
+++ commands/Gambling/makeitem.js
@@ -48,13 +48,13 @@ module.exports = {
             {upsert:true}
         );
 
-        console.log("저장 끝")
+        console.log(gambling_Schema)
 
         const embed = new EmbedBuilder()
             .setDescription(
                 `**아이템이 생성/바뀌게 되었습니다. 이름: ${
-                    (gambling_find.hashtags[gambling_find.hashtags.length - 1].name)
-                }, 레벨: ${gambling_find.hashtags[gambling_find.hashtags.length - 1].value}**`
+                    (gambling_Schema.hashtags[gambling_Schema.hashtags.length - 1].name)
+                }, 레벨: ${gambling_Schema.hashtags[gambling_Schema.hashtags.length - 1].value}**`
             )
             .setColor("Green");
         
