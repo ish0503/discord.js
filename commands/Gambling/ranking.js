96433219397ca118b357976aabc6a0feb1a7d5fa
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-08 18:57:34 +0900

    Add files via upload

---
+++ commands/Gambling/ranking.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const gambling_Schema = require("../../models/Money")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("순위")
+    .setDescription("새냥신께서 주신 돈이 누가 제일 많은지 봅니다."),
+
+    /**
+     * 
+     * @param {import(*discord.js*).ChatInputCommandInteraction} interaction
+     */
+    async execute(interaction){
+        const gambling_find = await gambling_Schema
+        .find()
+        .sort([["money","descending"]])
+        .limit(10)
+        .exec();
+
+        const embed = new EmbedBuilder()
+        .setTitle(`${interaction.client.user.username} 재화 순위 💰`)
+        .setColor("Green")
+        .setThumbnail(interaction.client.user.displayAvatarURL());
+
+        for (let i = 0; i < gambling_find.length; i++){
+            const user = await interaction.client.users.fetch(
+                gambling_find[i].userid
+            )
+            embed.addFields({
+                name: `${i + 1}, ${user.username}`,
+                value: `${interaction.client.user.username} 돈 : ${gambling_find[i].money.toLocaleString()}재화`
+            })
+        }
+
+        interaction.reply({embeds : [embed]})
+    }
+}
