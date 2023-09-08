3c3ae690dbba7c5fc2332f6432d26fa154f42b73
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-08 23:09:44 +0900

    Create upgrade.js

---
+++ models/upgrade.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const SchemaF = new Schema({
+  userid: String,
+  hashtags: [
+    { name: String, value: Number },
+    { name: String, value: Number },
+    { name: String, value: Number },
+  ],
+  cooltime: String,
+})
+
+module.exports = model("Upgrade", SchemaF);
