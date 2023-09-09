a9d06101e9c2a7766ebfc43105273e1ba8a9ede5
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-09 09:18:04 +0900

    Update Checkitems.js

--- commands/Gambling/Checkitems.js
+++ commands/Gambling/Checkitems.js
@@ -34,7 +34,7 @@ module.exports = {
           console.log(v)
         }
         let count = 0
-        for (let i = 0; i > Object.keys(gambling_find.hashtags).length; i++){
+        for (let i = 0; i > gambling_find.hashtags.length; i++){
             console.log("반복"+count)
             console.log(gambling_find.hashtags[count])
             if (!gambling_find.hashtags[count]) { 
