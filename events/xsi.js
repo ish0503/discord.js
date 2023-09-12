991b5c582ca96bf2c79c51b8ac3d36bb06811de3
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-13 07:56:19 +0900

    Update xsi.js

--- events/xsi.js
+++ events/xsi.js
@@ -5,13 +5,11 @@ module.exports = {
     const Schema = require("../models/learning")
     const args = interaction.content.slice(1).trim().split(/ +/) 
     const argsjoin = args.join(" ")
-    console.log(argsjoin)
     const ff = await Schema.findOne({ word: argsjoin })
     if (ff && interaction.content.substr(0, 1) == "야") {
       const meaning = ff.meaning
       //let user = userid//interaction.member
       if (!ff.userid) userid = "0"
-      console.log(`${meaning} @${ff.userid}님이 알려주셨어요!`)
       interaction.channel.send(`**${meaning}** (<@${ff.userid}>님이 알려주셨어요!)`)
       //interaction.channel.recipientId(`\`${ff.meaning}``\n${user.tag || user}님이 알려주셨어요 !`)
       }
