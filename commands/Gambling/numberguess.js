c2d62241bdc2b4cea42fcad099c8d82b49aee0b0
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-13 17:51:19 +0900

    Add files via upload

--- commands/Gambling/numberguess.js
+++ commands/Gambling/numberguess.js
@@ -1,9 +1,10 @@
-const { SlashCommandBuilder , EmbedBuilder } = require("discord.js")    
+const { SlashCommandBuilder , EmbedBuilder } = require("discord.js")  
+const comma = require("comma-number")
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("미니게임")
-        .setDescription("미니게임을 시작합니다."),
+        .setName("숫자맞추기")
+        .setDescription("숫자맞추기를 시작합니다."),
     /**
      * 
      * @param {import("discord.js").CommandInteraction} interaction
@@ -11,15 +12,28 @@ module.exports = {
      * 
      */
     async execute(interaction) {
-        const embed = new EmbedBuilder()
-        .setTitle('미니게임')
-        .setDescription('미니게임을 시작합니다.')
-        .setColor('2F3136')
-        interaction.reply({embeds: [embed], ephemeral: true});
         var channel = `1148907220889780225`
         if (interaction.channel.id == channel) {
+            const embed = new EmbedBuilder()
+            .setTitle('숫자맞추기')
+            .setDescription('숫자맞추기를 시작합니다. 숫자를 말해주세요.')
+            .setColor('2F3136')
+            interaction.reply({embeds: [embed], ephemeral: true});
+
+            const gambling_Schema = require("../../models/Money")  
+            const gambling_find = await gambling_Schema.findOne({
+                userid:interaction.user.id
+            })
+
             function random(min, max) {
                 return Math.floor(Math.random() * (max - min)) + min;
+            }
+            async function changemoney(money){
+                await gambling_Schema.updateOne(
+                    {userid: interaction.user.id},
+                    {money: (gambling_find.money || 1) + money},
+                    {upsert:true}
+                );
             }
             var num = random(1, 100)
             var count = 0
@@ -37,7 +51,10 @@ module.exports = {
                 }
                 if (number == num) {
                     count = count + 1
-                    interaction.followUp({content: `정답입니다! ${count}번 만에 맞추셨습니다!`, ephemeral: true});
+                    const money = Math.round(25000 / count)
+                    changemoney(money)
+                    interaction.followUp({content: `정답입니다! ${count}번 만에 맞추셨습니다!\n${comma(money)}재화가 들어왔습니다!`, ephemeral: true});
+
                     collector.stop()
                 }
             });
@@ -47,6 +64,12 @@ module.exports = {
                 }
             }
             );
+        }else{
+            const embed = new EmbedBuilder()
+            .setTitle('오류')
+            .setDescription('해당 명령어는 새냥교 서버의 봇 채널에서만 가능합니다.\n새냥교 서버에서 써주세요. (사이트 : /정보)')
+            .setColor('964242')
+            interaction.reply({embeds: [embed], ephemeral: true});
         }
     }
 };
