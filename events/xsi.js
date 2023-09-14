51f72d289e04c173507a0344b11a27eaa8369498
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-15 08:03:29 +0900

    Update xsi.js

--- events/xsi.js
+++ events/xsi.js
@@ -10,7 +10,7 @@ module.exports = {
       const meaning = ff.meaning
       //let user = userid//interaction.member
       if (!ff.userid) userid = "0"
-      const User = client.users.cache.get(ff.userid); // Getting the user by ID.
+      const User = interation.users.cache.get(ff.userid); // Getting the user by ID.
       if (User) { // Checking if the user exists.
         console.log("유저찾음")
              interaction.channel.send(`**${meaning}** (<@${User.tag}>님이 알려주셨어요!<:Heart:1151445619215441980>)`)
