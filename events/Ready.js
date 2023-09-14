cdced1224c0d640e0ab65f84662e40d3212681cd
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-14 18:28:34 +0900

    Update Ready.js

--- events/Ready.js
+++ events/Ready.js
@@ -5,14 +5,14 @@ module.exports = {
   once: true,
   execute(client) {
     let number = 0
-    // setInterval(() => {
-    //     const list = [`현재 ${client.guilds.cache.size}개의 서버에서 휴식`] 
-    //     if(number == list.length) number = 0
-    //     client.user.setActivity(list[number],{
-    //         type: Client.Playing
-    //     })
-    //     number++
-    // }, 10000)
+    setInterval(() => {
+        const list = [`현재 ${client.guilds.cache.size}개의 서버에서 휴식`] 
+        if(number == list.length) number = 0
+        client.user.setActivity(list[number],{
+            type: Client.Playing
+        })
+        number++
+    }, 10000)
     console.log(`${client.user.tag} 봇 이 준비되었습니다.`)
   },
 };
