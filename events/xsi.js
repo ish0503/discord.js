3d096684a789556733afe688999bfff3944e3bef
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-15 16:01:44 +0900

    Update xsi.js

--- events/xsi.js
+++ events/xsi.js
@@ -1,3 +1,5 @@
+const client = require("../index")
+
 module.exports = {
   name: "messageCreate",
   once: false,
@@ -10,13 +12,12 @@ module.exports = {
       const meaning = ff.meaning
       //let user = userid//interaction.member
       if (!ff.userid) userid = "0"
-      //const User = interation.users.cache.get(ff.userid); // Getting the user by ID.
-      //if (User) { // Checking if the user exists.
-        //console.log("유저찾음")
-             //interaction.channel.send(`**${meaning}** (<@${User.tag}>님이 알려주셨어요!<:Heart:1151445619215441980>)`)
-      //} else {
-          interaction.channel.send(`**${meaning}** (<@${ff.userid}>님이 알려주셨어요!<:Heart:1151445619215441980>)`)
-      //};
+      const username = client.users.cache.get(ff.userid);
+      if (username){
+        interaction.channel.send(`**${meaning}** (${username.username}[${username.globalName}]님이 알려주셨어요!<:Heart:1151445619215441980>)`)
+      }else{
+        interaction.channel.send(`**${meaning}** (<@${ff.userid}>]님이 알려주셨어요!<:Heart:1151445619215441980>)`)
+      }
       
       //interaction.channel.recipientId(`\`${ff.meaning}``\n${user.tag || user}님이 알려주셨어요 !`)
       }
