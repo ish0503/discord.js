62bfb303e15b7c01a5363764894cb05e1a7558b1
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-19 22:55:20 +0900

    Update xsi.js

--- events/xsi.js
+++ events/xsi.js
@@ -20,14 +20,14 @@ module.exports = {
       }
       
       //interaction.channel.recipientId(`\`${ff.meaning}``\n${user.tag || user}님이 알려주셨어요 !`)
-      }else if(message.content.startsWith("!골라")) {
-    const args = message.content.slice("!".length).trim().split(/ +/),
+      }else if(interaction.content.startsWith("!골라")) {
+    const args = interaction.content.slice("!".length).trim().split(/ +/),
       list = args.slice(1);
 
-    if (!list.length) return message.reply("선택지를 입력해주세요!");
+    if (!list.length) return interaction.reply("선택지를 입력해주세요!");
 
     const index = Math.floor(Math.random() * list.length);
 
-    message.reply(`저의 선택은 \`${list[index]}\` 입니다`);
+    interaction.reply(`저의 선택은 \`${list[index]}\` 입니다`);
   }
 }}
