7b420c7e4db7da1b4be07f1128aff7956a135e3c
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-30 00:27:17 +0900

    Update buystocks.js

--- commands/stock/buystocks.js
+++ commands/stock/buystocks.js
@@ -261,7 +261,7 @@ module.exports = {
             for (let i = 0; i < length; i++){
                 if (gambling_find.hashtags[i].name == args) {
                     isitem = i
-                    if (gambling_find.hashtags[i].value - value2 >= 0) {
+                    if (gambling_find.hashtags[i].value - value2 > 0) {
                         soondeleteitem.push({"name": gambling_find.hashtags[i].name, "value": gambling_find.hashtags[i].value - value2})
                     }else {
                         value2 = gambling_find.hashtags[i].value
