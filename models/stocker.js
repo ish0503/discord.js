85ae05611c7628405ceb9eb628437097d8a97612
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-09-30 13:18:31 +0900

    Update stocker.js

--- models/stocker.js
+++ models/stocker.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const SchemaF = new Schema({
   userid: String,
-  hashtags: [{ "name": String, "value": Number }],
+  hashtags: [{ "name": String, "value": Number, "lastvalue": Number }],
   cooltime: String,
 })
 
