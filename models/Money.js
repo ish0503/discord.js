0edeccf72f165bef17e1b97bd988838da955b8a4
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-01 09:47:02 +0900

    Add files via upload

--- models/Money.js
+++ models/Money.js
@@ -1,8 +1,9 @@
 const { Schema, model } = require("mongoose");
+const BigNumberSchema = require('mongoose-bignumber');
 
 const SchemaF = new Schema({
   userid: String,
-  money: Number,
+  money: BigNumberSchema,
   cooltime: String,
 })
 
