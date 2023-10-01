af84601f46fc57d039b966e073661bfb5bc3accc
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-01 11:04:23 +0900

    Add files via upload

--- commands/Gambling/출첵.js
+++ commands/Gambling/출첵.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder , EmbedBuilder } = require('discord.js');
-const 도박_Schema = require("../../models/출석체크")
+const _Schema = require("../../models/출석체크")
 const gambling_Schema = require("../../models/Money")
 
 module.exports = {
@@ -8,9 +8,7 @@ module.exports = {
         .setDescription("오늘 출첵을 합니다."),
     /**
      * 
-     * @param {import("discord.js").CommandInteraction} interaction
-     * @returns
-     * 
+     * @param {import(*discord.js*).ChatInputCommandInteraction} interaction
      */
     async execute(interaction, client) {
         let newData
@@ -18,12 +16,13 @@ module.exports = {
             userid:interaction.user.id
         })
         const t = new Date()
-        const user = await 도박_Schema.findOne({ userid: interaction.user.id })
+        const user = await _Schema.findOne({ userid: interaction.user.id })
         const date = "" + t.getFullYear() + (t.getMonth() + 1) + t.getDate()
                 if (!user) {
-                    await 도박_Schema.updateOne(
+                    await _Schema.updateOne(
                     {userid: interaction.user.id},
-                    { count: 1, date: date }
+                    { count: 1, date: date },
+                    {upsert:true}
                     )
                     await gambling_Schema.updateOne(
                         {userid: interaction.user.id},
@@ -33,7 +32,7 @@ module.exports = {
                     const embeds = new EmbedBuilder()
                     .setTitle("출석체크를 완료했어요.")
                     .setDescription(`1번째 <@${interaction.member.user.id}> 출석체크 완료! (+ 10000 재화)`)      
-                    .setColor(`#113131`)
+                    .setColor(`Blue`)
                     .setTimestamp()
                     //.setFooter(`${interaction.member.user.tag}`, interaction.member.user.displayAvatarURL()) 
                     interaction.reply({embeds: [embeds]})
@@ -42,17 +41,17 @@ module.exports = {
                     const embedss = new EmbedBuilder()
                     .setTitle("출석 체크 이미 완료. 내일 또 와요 !")
                     .setDescription(`<@${interaction.member.user.id}>님은 이미 출석을 한 상태입니다.`)
-                    .setColor(`#2424242`)
+                    .setColor(`Red`)
                     .setTimestamp()
                     //.setFooter(`${interaction.member.user}`, interaction.member.user.displayAvatarURL()) 
                     if (user.date == date) return interaction.reply({embeds: [embedss]})
-                    await Schema.findOneAndRemove({ userid: interaction.user.id })
-                    await 도박_Schema.updateOne(
+                    await _Schema.updateOne(
                         {userid: interaction.user.id },
                         { 
                         count: parseInt(user.count) + 1,
                         date: date 
-                    })
+                    },
+                    {upsert:true})
                     await gambling_Schema.updateOne(
                         {userid: interaction.user.id},
                         {money: (gambling_find?.money || 0) + random, cooltime: gambling_find?.cooltime || 0},
@@ -61,7 +60,7 @@ module.exports = {
                     const embedsss = new EmbedBuilder()
                     .setTitle("출석체크를 완료했어요.")
                     .setDescription(`${parseInt(user.count) + 1}번째 <@${interaction.member.user.id}> 출석체크 완료! (+ ${random} 재화)`)
-                    .setColor(`#242422`)
+                    .setColor(`Green`)
                     .setTimestamp()
                     .setFooter(`${interaction.member.user.tag}`, interaction.member.user.displayAvatarURL()) 
                     interaction.reply({embeds: [embedsss]})
