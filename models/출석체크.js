ea527d3b7f685a555f6712b64a7f4a600d4490b6
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-01 10:48:38 +0900

    Add files via upload

--- models/출석체크.js
+++ models/출석체크.js
@@ -2,8 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const SchemaF = new Schema({
   userid: String,
-  date: Number,
   count: Number,
+  date: String,
 })
 
 module.exports = model("출석체크", SchemaF);
