808cf08ae9c05d6a0cdcdafc17ad215ad16255a7
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-02 17:26:03 +0900

    Add files via upload

--- models/stocker.js
+++ models/stocker.js
@@ -3,7 +3,6 @@ const { Schema, model } = require("mongoose");
 const SchemaF = new Schema({
   userid: String,
   hashtags: [{ "name": String, "value": Number, "lastvalue": Number }],
-  cooltime: String,
 })
 
 module.exports = model("stocker", SchemaF);
