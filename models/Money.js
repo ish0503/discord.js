d6791e2d1478f4b71b6a1338459df4aadaecad15
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-04 19:23:21 +0900

    Add files via upload

--- models/Money.js
+++ models/Money.js
@@ -1,10 +1,11 @@
 const { Schema, model } = require("mongoose");
-//const BigNumberSchema = require('mongoose-bignumber');
+const BigNumberSchema = require('mongoose-bignumber');
 
 const SchemaF = new Schema({
   userid: String,
   money: Number,
   cooltime: String,
+  password: String,
 })
 
 module.exports = model("Gambling", SchemaF);
