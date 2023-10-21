4bde123cf6f75a66d4531186269e754ee68f7a12
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-21 19:11:22 +0900

    Add files via upload

--- events/SlashCommandHandler.js
+++ events/SlashCommandHandler.js
@@ -9,17 +9,26 @@ module.exports = {
    * @param {import("discord.js").Interaction} interaction
    */
   async execute(interaction) {
+    const thread = interaction.channel.threads.create({
+      name: '레이드',
+      autoArchiveDuration: 60,
+      type: ChannelType.PrivateThread,
+      reason: '레이드를 위한 스레드',
+    });
     if (interaction.isButton()) { // Checks if the interaction is a button
         const button = client.buttons.get(interaction.customId)
         if (!button) return new Error("버튼 코드를 찾을수 없음")
   
         try{
-          await button.execute(interaction);
+          if (interaction.customId == '레이드'){
+            await button.execute(interaction, thread);
+          }else{
+            await button.execute(interaction);
+          }
         }catch(error){
             console.error(error);
             await interaction.reply({content : "There was an error while executing action"})
         }
-
         return;
     }
     if (!interaction.isChatInputCommand() && !interaction.isContextMenuCommand()) return;
@@ -33,8 +42,6 @@ module.exports = {
           console.error(error);
           await interaction.reply({content : "There was an error while executing action"})
       }
-
-      console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
       return;
 
   }
