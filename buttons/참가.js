ff5516f09ac5036f9054d3102cb297431f7a298e
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-21 19:11:40 +0900

    Add files via upload

---
+++ buttons/참가.js
@@ -0,0 +1,27 @@
+const gambling_Schema = require("../models/Money")
+const gambling_Schema2 = require("../models/upgrade")
+const level_Sechma = require("../models/level")
+
+const { } = require("discord.js")
+
+participate = []
+
+module.exports = {
+    data:{
+        name: "참가",
+    },
+        /**
+     *
+     * @param {import("discord.js").Interaction} interaction
+     */
+    async execute(interaction, thread) {
+        console.log(interaction.user.id)
+        if (participate.find(interaction.user.id)) {
+            interaction.reply({content: '이미 레이드에 참가해있거나 쿨타임 시간입니다.', ephemeral: true}) 
+            return;
+        }
+        participate.push(interaction.user.id)
+
+        thread.join(await thread.members.add(interaction.user.id))
+    }
+}
