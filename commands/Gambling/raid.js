f2ea1875eaabfd84c26fe32c3e2b90b96d98cccc
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-21 21:00:38 +0900

    Update raid.js

--- commands/Gambling/raid.js
+++ commands/Gambling/raid.js
@@ -18,6 +18,19 @@ const { table } = require("node:console");
      */
     async execute(interaction) {
         await interaction.deferReply()
+	    if (const thread2 = interaction.channel.threads.cache.find(x => x.name === '레이드')){
+		    await interaction.editReply({
+                       content: `이 채널에서 이미 레이드가 진행중입니다.?`,
+                   });
+		    return;
+	    }
+
+	 const thread = interaction.channel.threads.create({
+          name: '레이드',
+          autoArchiveDuration: 60,
+          type: ChannelType.PrivateThread,
+          reason: '레이드를 위한 스레드',
+         });
 
         const confirm = new ButtonBuilder()
 			.setCustomId(`참가`)
@@ -44,5 +57,7 @@ const { table } = require("node:console");
         interaction.channel.send("이제 시작합니다.")
 
         interaction.deleteReply()
+
+	
     },
   };
