2ad0d8c5ca6b45604e26f43405064f592dc76838
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-21 17:12:45 +0900

    Add files via upload

--- commands/Gambling/checkmoney.js
+++ commands/Gambling/checkmoney.js
@@ -4,7 +4,7 @@ const heart_Sechma = require("../../models/level")
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("정보확인")
+    .setName("돈확인")
     .setDescription("당신의 돈의 총액을 확인할 수 있습니다."),
 
     /**
@@ -16,7 +16,7 @@ module.exports = {
             userid:interaction.user.id
         })
 
-        const heart_find = await heart_Sechma({
+        const heart_find = await heart_Sechma.findOne({
             userid:interaction.user.id
         })
 
@@ -31,7 +31,7 @@ module.exports = {
             `**${
                 interaction.user
             }님의 재화는 총 ${Number(gambling_find.money).toLocaleString()}입니다.**\n
-            lv: ${Number(heart_find?.level || 1).toLocaleString()}, xp: ${Number(heart_find?.exp || 0).toLocaleString()}`
+            lv: ${Number(heart_find.level).toLocaleString()}, xp: ${Number(heart_find.exp).toLocaleString()}`
         ).setColor("Green")
 
         interaction.reply({embeds: [embed]})
