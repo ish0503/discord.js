96a635e85671704e5338dc3a6679b34b037798b2
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-22 22:20:56 +0900

    Update upgrade.js

--- commands/Upgrade/upgrade.js
+++ commands/Upgrade/upgrade.js
@@ -96,6 +96,12 @@ module.exports = {
             }
         }
         let length = gambling_find.hashtags.length
+	if (length >= 5){
+                interaction.reply({
+                    content: `**아이템을 5개 넘게 보유할 수 없습니다.**`
+                });
+                return;
+	}
         let isitem = -1
         let hasitem = []
         
