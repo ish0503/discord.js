1f0bd81f404bb9a5d1d1a6cf57c0277ef71c2c98
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-22 10:43:36 +0900

    Add files via upload

--- models/raidparty.js
+++ models/raidparty.js
@@ -1,7 +1,8 @@
 const { Schema, model } = require("mongoose");
 
 const SchemaF = new Schema({
-  hashtags : [{ "channelid": String, "userid": [String] }],
+    "channelid": String,
+    "userid": [String]
 })
 
 module.exports = model("raidparty", SchemaF);
