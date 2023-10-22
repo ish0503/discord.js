22e432430af630ec57045bfd3b0b5961d7050596
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-22 10:02:11 +0900

    Add files via upload

--- buttons/참가.js
+++ buttons/참가.js
@@ -12,7 +12,7 @@ module.exports = {
      */
     async execute(interaction) {
         const raid = await raid_Sechma.findOne({
-            "hashtags.channelid": interaction.channel.id
+            channelid: interaction.channel.id
         })
 
         if (!raid){
@@ -23,31 +23,15 @@ module.exports = {
             return;
         }
 
-        var list = []
+        var list = raid.userid
 
-        console.log(interaction.user.id)
-        console.log(raid)
-        console.log(raid.hashtags)
-        var isitem = -1
-        for (let i = 0; i < raid.hashtags.length; i++){
-            list.push(raid.hashtags[i])
-            if (raid.hashtags[i].channelid == interaction.channel.id) {
-                isitem = i
-            }
-        }
+        list.push(interaction.user.id)
 
-        if (!list[isitem].userid == interaction.user.id){
-            console.log(list[isitem])
-
-            list[isitem].userid.push(interaction.user.id)
-        }
         // if (participate.find(interaction.user.id)) {
         //     interaction.reply({content: '이미 레이드에 참가해있거나 쿨타임 시간입니다.', ephemeral: true}) 
         //     return;
         // }
         // participate.push(interaction.user.id)
-        const thread2 = interaction.channel.threads.cache.find(x => x.name === '레이드');
-        await thread2.members.add(interaction.user.id);
         interaction.reply({
             content: '성공적으로 참가되었습니다.',
             ephemeral: true
