417350d566977f9a2f7927e3c26a7ca247f23a25
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-22 09:22:50 +0900

    Add files via upload

--- commands/Gambling/raid.js
+++ commands/Gambling/raid.js
@@ -1,65 +1,64 @@
 const {
-    SlashCommandBuilder,
-    ButtonBuilder,
-    ActionRowBuilder,
-    ButtonStyle,
-    ChannelType,
-  } = require("discord.js");
-  const comma = require("comma-number");
+  SlashCommandBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+  ChannelType,
+} = require("discord.js");
+const comma = require("comma-number");
 const { table } = require("node:console");
-  const wait = require('node:timers/promises').setTimeout;
-  
-  module.exports = {
-    data: new SlashCommandBuilder()
-      .setName("레이드")
-      .setDescription("레이드를 시작해 보스 몬스터에 도전하세요."),
-    /**
-     *
-     * @param {import("discord.js").ChatInputCommandInteraction} interaction
-     */
-    async execute(interaction) {
-        await interaction.deferReply()
-	    if (interaction.channel.threads.cache.find(x => x.name === '레이드')){
-		    await interaction.editReply({
-                       content: `이 채널에서 이미 레이드가 진행중입니다.?`,
-                   });
-		    return;
-	    }
+const wait = require('node:timers/promises').setTimeout;
 
-	 const thread = interaction.channel.threads.create({
-          name: '레이드',
-          autoArchiveDuration: 60,
-          type: ChannelType.PrivateThread,
-          reason: '레이드를 위한 스레드',
-         });
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("레이드")
+    .setDescription("레이드를 시작해 보스 몬스터에 도전하세요."),
+  /**
+   *
+   * @param {import("discord.js").ChatInputCommandInteraction} interaction
+   */
+  async execute(interaction) {
+      await interaction.deferReply()
+    if (interaction.channel.threads.cache.find(x => x.name === '레이드')){
+      await interaction.editReply({
+                     content: `이 채널에서 이미 레이드가 진행중입니다.`,
+                 });
+      return;
+    }
 
-        const confirm = new ButtonBuilder()
-			.setCustomId(`참가`)
-			.setLabel(`참가`)
-			.setStyle(ButtonStyle.Primary);
+ const thread = interaction.channel.threads.create({
+        name: '레이드',
+        autoArchiveDuration: 60,
+        type: ChannelType.PrivateThread,
+        reason: '레이드를 위한 스레드',
+       });
 
-        const row = new ActionRowBuilder()
-            .addComponents(confirm);
+      const confirm = new ButtonBuilder()
+    .setCustomId(`참가`)
+    .setLabel(`참가`)
+    .setStyle(ButtonStyle.Primary);
 
-        // const thread = await interaction.channel.threads.create({
-         //   name: '레이드',
-           //  autoArchiveDuration: 60,
-     //        type: ChannelType.PrivateThread,
-  //           reason: '레이드를 위한 스레드',
-    //    });
+      const row = new ActionRowBuilder()
+          .addComponents(confirm);
 
-        await interaction.editReply({
-            content: `참가하시겠습니까?`,
-            components: [row],
-        });
+      // const thread = await interaction.channel.threads.create({
+       //   name: '레이드',
+         //  autoArchiveDuration: 60,
+   //        type: ChannelType.PrivateThread,
+//           reason: '레이드를 위한 스레드',
+  //    });
 
-        await wait(60000)
+      await interaction.editReply({
+          content: `참가하시겠습니까?`,
+          components: [row],
+      });
 
-        interaction.channel.send("이제 시작합니다.")
+      await wait(60000)
 
-        interaction.deleteReply()
-        
+      interaction.channel.send("이제 시작합니다.")
 
-    },
-  };
-2
+      interaction.deleteReply()
+      
+
+  },
+};
