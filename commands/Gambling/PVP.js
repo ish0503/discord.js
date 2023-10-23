734baabbc9a9e7d135dc529a503d76e7ab83198d
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-23 19:25:32 +0900

    Add files via upload

--- commands/Gambling/PVP.js
+++ commands/Gambling/PVP.js
@@ -181,13 +181,13 @@ const { table } = require("node:console");
                 monster.hp -= damage;
             }
             await wait(2000);
-            if (Math.random() * 100 < 10){
+            if (Math.random() * 100 < 20){
                 interaction.editReply(`*${args.user.username}*는 ${user.name}을(를) 공격합니다. {크리티컬!} ${damage * 2}대미지! \n(${args.user.username}: ${monster.hp}HP) (당신: ${user.hp - damage * 2}HP)`);
                 user.hp -= damage * 2;
-            }else if (Math.random() * 100 < 3){
+            }else if (Math.random() * 100 < 2){
                 interaction.editReply(`*${args.user.username}*의 공격이 빗나갔다! 0대미지. \n(${args.user.username}: ${monster.hp}HP) (당신: ${user.hp}HP)`);
                 user.hp -= 0;
-            }else if (Math.random() * 100 < 1){
+            }else if (Math.random() * 100 < 2){
                 interaction.editReply(`__**{회심의 일격!}**__ *${args.user.username}*는 ${user.name}을(를) 공격합니다. ${damage * 10}대미지! \n(${args.user.username}: ${monster.hp}HP) (당신: ${user.hp - damage * 10}HP)`);
                 user.hp -= damage * 10;
             }else{
