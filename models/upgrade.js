eb8dd4632c2826f1ad7c6a05fae27109cd580513
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-10-24 16:20:36 +0900

    Add files via upload

--- models/upgrade.js
+++ models/upgrade.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 const SchemaF = new Schema({
   userid: String,
   hashtags : [{ "name": String, "value": Number }],
+  skills : [{ "name": String, "Lv": Number }],
   cooltime: String,
   defense: Number,
 })
