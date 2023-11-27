ad296c63a2ad86d16378db0774230b82370ac7ac
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-11-27 21:51:30 +0900

    Update xsi.js

--- events/xsi.js
+++ events/xsi.js
@@ -13,14 +13,14 @@ module.exports = {
       return;
     }
     let msg = interaction.content.toLowerCase();
-    for (x = 0; x < profanities.length; x++) {
+    //for (x = 0; x < profanities.length; x++) {
         if ( interaction.author.id === TARGET_USER_ID) {
     // 메시지 삭제
     message.delete()
       .then(() => console.log(`Deleted message from ${message.author.tag}: ${message.content}`))
       .catch(console.error);
   }
-}
+//}
  //if ((msg.includes("ㅄ")) || (msg.includes("좃")) || (msg.includes("ㅗ")) || (msg.includes("새") && msg.includes("끼")) || (msg.includes("시") && msg.includes("발")) || (msg.includes("씨") && msg.includes("발")) || (msg.includes("병") && msg.includes("신")) || (msg.includes("ㅈ")) || (msg.includes("섹") && msg.includes("스")) || (msg.includes("ㅅ")) || (msg.includes("ㅆ")) || (msg.includes("좆")) || (msg.includes("조") && msg.includes("까")) || (msg.includes("븅") && msg.includes("신")) ){
           //  let msg2 = await interaction.reply("여기서 이 단어를 말할 수 없습니다!") 
             //  if (interaction && msg2){
