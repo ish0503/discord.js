9f5a3006aa9964872f76f67ebd1351ed3d6ef762
ish0503 <98135486+ish0503@users.noreply.github.com>
2023-12-06 15:03:55 +0900

    Add files via upload

---
+++ commands/Gernal/meme.js
@@ -0,0 +1,55 @@
+const { default: axios } = require("axios");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("랜덤짤")
+    .setDescription("랜덤 짤을 보여줍니다")
+    .addStringOption((f) =>
+      f
+        .setName("종류")
+        .setDescription("짤 종류를 선택해 주세요")
+        .setRequired(true)
+        .addChoices(
+            { name: "wife", value: "waifu" },
+            { name: "네코", value: "neko" },
+            { name: "죽여", value: "kill" },
+            { name: "키스", value: "kiss" },
+            { name: "행복", value: "happy" },
+            { name: "댄스", value: "dance" },
+            { name: "허그", value: "hug" },
+            { name: "쓰다듬기", value: "pat" },
+            { name: "인사", value: "wave" },
+            { name: "울음", value: "cry" },
+            { name: "윙크", value: "wink" }
+        )
+    ),
+  /**
+   *
+   * @param {import("discord.js").ChatInputCommandInteraction} interaction
+   */
+  async execute(interaction) {
+    interaction.deferReply({ ephemeral: true })
+    const category = interaction.options.getString("종류");
+
+    let get_meme = [];
+
+    try {
+        for (let i = 1; i < 10; i++){
+            get_meme.push((await axios.get(`https://api.waifu.pics/sfw/${category}`)).data)
+        }
+    } catch (error) {}
+
+    interaction.editReply({ content: "BOOM!", ephemeral: true });
+
+    for (let i = 1; i < 10; i++){
+        const embed = new EmbedBuilder()
+        .setTitle("랜덤 짤")
+        .setColor("Random")
+        .setImage(get_meme.url)
+        .setFooter({ text: "powered by waifu.pics" });
+
+        interaction.channel.send({ embeds: [embed], ephemeral: true });
+    }
+  },
+};
